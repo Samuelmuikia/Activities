967e6b537bfab90e1acc5394875231431892102a
samuel <shopifyworld28@gmail.com>
2022-04-12 21:01:59 -0700

    message

---
+++ week2/JSVariablesAndPrimitive.js
@@ -0,0 +1,33 @@
+// create a constant variable (const)
+const tom = "string";
+
+// create a variable that can be reassigned (let)
+let tree;
+
+// create three variables and assign them values of different data types (=)
+
+let aNum = 7e7;
+let story = 'we can tell a story as part of this variable value';
+let theTruth = false;
+
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof aNum);
+console.log(typeof story) ;
+
+// change the value referenced by a variable (dynamic typing)
+story = 999;
+
+// print the type of the variable you just changed (typeof)
+console.log(typeof story);
+
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+let name = "john";
+let language = "Javascript";
+let number = 2;
+let time = false;
+
+console.log("Hello, my name is " +  name + ", and I am learning " +  language + ". I have been practicing for only " +  number + " weeks, so my status as a master is: " +  false + ".");
