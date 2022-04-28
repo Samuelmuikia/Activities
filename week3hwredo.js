a2e3827b8bbb93f544e318c0eff68a0130f3650a
samuel <shopifyworld28@gmail.com>
2022-04-27 23:52:03 -0700

    week3hwredo.js

---
+++ week3hwredo.js
@@ -0,0 +1,35 @@
+//
+const pizzToppings = ["pepperoni", "bacon", "sausage", "lemon"]
+function greetCustomer(Toppings) {
+  let greeting = (`Welcome to johnny's pizza we offer`);
+   for (let topping of  toppings)
+    greeting += ` ${Topping}`
+   console.log (greeting);
+};
+greetCustomer(pizzaToppings);
+
+function getPizzaOrder (size, crust, ...Toppings) {
+ let order = `One $(size) $(crust) pizza with` ;
+ for (let toping of  toppings)
+  order += ` ${Topping}`
+console.log(pizzaOrder ("coming up");
+}
+return [size, crust, toppings];
+order += `coming up`
+getPizzaOrder(`large`, `stuffed crust`, `bacon`, `sausage`, `pepperoni`, `lemons`)
+
+function preparePizza ["size", "crust", "...Toppings"]
+let pizzaObject = {One $(size) $(crust) pizza with} ;
+for (let toping of  toppings)
+pizzaObject += ` ${Topping}`
+console.log(preparePizza("...Cooking pizza...");
+pizzaObject[];
+return ["size", "crust", "...Toppings"];
+
+function servePizza {pizza}
+let pizza = ("Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!");
+for (let toping of  toppings)
+pizza += ` ${Topping}`
+console.log(servePizza(pizza);
+pizza[];
+return [size, crust, toppings];
